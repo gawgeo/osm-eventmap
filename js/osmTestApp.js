3624c03be6dc67a11bc81e175a6f79ae10e20200
Tobias Domnik <tobias.domnik@kit.edu>
2016-04-05 18:09:21 +0200

    include leaflet js und eine einfache map von Karlsruhe

--- js/osmTestApp.js
+++ js/osmTestApp.js
@@ -1,3 +1,17 @@
-/**
- * Created by qo8779 on 01.04.2016.
- */
+angular.module('osmTestApp', [])
+    //use strict
+    .controller('osmTestAppCtrl', function () {
+        console.log("OSM-Test App running!");
+        // OSM imports and settings
+
+        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+        var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+        var osm = new L.TileLayer(osmUrl, {minZoom: 5, maxZoom: 18, attribution: osmAttrib});
+
+        // create map with center in Karlsruhe
+        var map = new L.Map('simpleMap');
+        map.addLayer(osm); // Layer server hinzufügen
+        map.setView(new L.LatLng(49.0148731,8.4191506),14); // Position laden
+
+    });
+
