b8853fb7963a05f9a7b13a68dd1c22e28e63a887
Tobias Domnik <tobias.domnik@kit.edu>
2016-04-07 16:08:48 +0200

    add one marker

--- js/osmTestApp.js
+++ js/osmTestApp.js
@@ -1,17 +1,22 @@
 angular.module('osmTestApp', [])
-    //use strict
-    .controller('osmTestAppCtrl', function () {
-        console.log("OSM-Test App running!");
-        
-            // OSM imports and settings
-        var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-        var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-        var osm = new L.TileLayer(osmUrl, {minZoom: 5, maxZoom: 18, attribution: osmAttrib});
+  //use strict
+  .controller('osmTestAppCtrl', function () {
+      console.log("OSM-Test App running!");
 
-        // create map with center in Karlsruhe
-        var map = new L.Map('simpleMap');
-        map.addLayer(osm); // Layer server hinzufügen
-        map.setView(new L.LatLng(49.0148731,8.4191506),14); // Position laden
+      // OSM imports and settings
+      var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+      var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+      var osm = new L.TileLayer(osmUrl, {minZoom: 5, maxZoom: 18, attribution: osmAttrib});
 
-    });
+      // create map with center in Karlsruhe
+      var map = new L.Map('simpleMap'); // Map in <div> element mit dem Namen 'simpleMap' laden
+      map.addLayer(osm); // Layer server hinzufügen
+      map.setView(new L.LatLng(49.0148731, 8.4191506), 14); // Position laden
+
+      // ad marker to layer
+      var marker = L.marker([49.0148731, 8.4191506]);
+      map.addLayer(marker);
+
+
+  });
 
