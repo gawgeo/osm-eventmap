174bc9b09327fb22d2b4b9b6869fde94c2531947
Tobias Domnik <tobias.domnik@kit.edu>
2016-04-13 17:05:51 +0200

    add some functionality

--- js/osmTestApp.js
+++ js/osmTestApp.js
@@ -1,6 +1,6 @@
 angular.module('osmTestApp', [])
   //use strict
-  .controller('osmTestAppCtrl', ['$scope',  function ($scope) {
+  .controller('osmTestAppCtrl', function ($scope, $compile) {
       console.log("OSM-Test App running!");
 
       // OSM imports and settings
@@ -13,11 +13,7 @@ angular.module('osmTestApp', [])
       map.addLayer(osm); // Layer server hinzufügen
       map.setView(new L.LatLng(49.0148731, 8.4191506), 14); // Position laden
 
-      // add one marker to map
-      var marker = L.marker([49.0148731, 8.4191506]);
-      map.addLayer(marker);
-
-      // add circle to map
+      // add circle
       var circle = L.circle([49.0148731, 8.43000], 500, {
           color: 'red',
           fillColor: '#f03',
@@ -25,19 +21,65 @@ angular.module('osmTestApp', [])
       });
       map.addLayer(circle);
 
-      // add polygone by click
+      // add polygon
       var polygon = L.polygon([
           [49.005, 8.41000],
           [49.013, 8.42000]
       ]);
       map.addLayer(polygon);
 
-      //add marker by click
-      function onMapClick(event) {
-          var newMarker = L.marker(event.latlng);
-          map.addLayer(newMarker);
-          console.log("New Marker added: ", event.latlng);
+      // => marker add und delete handling inside group <=
+      var markerGroup = L.layerGroup();
+
+      // add one marker by click
+      map.on('click', function (event) {
+          var linkFn = $compile('<button ng-click="deleteThis()">Delete!</button>');
+          var content = linkFn($scope);
+          var marker = L.marker(event.latlng).bindPopup(content[0]).on("popupopen", function () {
+              var currentMarker = this;
+              $scope.deleteThis= function () {
+                  $scope.deleteMarker(currentMarker);
+              }
+          });
+          markerGroup.addLayer(marker);
+          console.log("Marker added: ", marker);
+          map.addLayer(markerGroup);
+          updateView();
+      });
+
+      // delete one marker by popup
+      $scope.deleteMarker = function (thisMarker) {
+          markerGroup.removeLayer(thisMarker);
+      };
+
+      // delete all marker
+      $scope.clearMarker = function () {
+          markerGroup.clearLayers();
+      };
+
+      // delete all layers
+      $scope.clear = function () {
+          map.removeLayer(polygon).removeLayer(circle);
+          $scope.clearMarker();
+      };
+
+      // update view
+      function updateView () {
+          $scope.$apply();
       }
-      map.on('click', onMapClick);
-  }]);
+
+      // Layer controls
+      var overlay = {
+          "polygon": polygon,
+          "circle": circle,
+          "marker": markerGroup
+      };
+      L.control.layers([], overlay).addTo(map);
+
+      
+      // Scope variables für außerhalb der Map
+      $scope.layers = map._layers; //Übersicht über alle Layer der map
+      $scope.markers = markerGroup._layers;
+
+  });
 
